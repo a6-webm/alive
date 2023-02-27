c4c9a716ca8bf219582f953fb746d9961f166938
a6.webm <46289515+a6-webm@users.noreply.github.com>
2023-02-27 12:24:24 +0000

    somethin idk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+type Tick = u64;
+type PId = u32;
+
+enum GndTile {
+    Water,
+    Grass,
+}
+
+enum Tile {
+    Empty,
+    Pin(PId),
+}
+
+struct Pin {
+    id: PId,
+    loc: (i32, i32),
+}
+
+struct World {
+    gnd: Vec<Vec<GndTile>>,
+    w: Vec<Vec<Tile>>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let world: Vec<Vec<&dyn Tiler>> = Vec::new();
 }
